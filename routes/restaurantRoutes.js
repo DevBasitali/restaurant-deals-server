a3e3a6fb41773bc262ea982b105f497b1e47573b
devBasitali <rajaalix2022@gmail.com>
2025-03-29 22:40:09 +0500

    working on restaurant module

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const restaurantController = require('../controller/restaurantController');
-const { authMiddleware, approvedRestaurantMiddleware } = require("../middleware/authMiddleware");
+const { approvedRestaurantMiddleware, authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/add-restaurant", authMiddleware, approvedRestaurantMiddleware, restaurantController.createRestaurant);
 
